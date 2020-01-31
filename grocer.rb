2c1dabdaf8e0dd50a88c90392a300af83ca0a95a
Jonathan Higger <jonathan.higger@gmail.com>
2020-01-31 00:05:32 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,19 +1,16 @@
 require "pry"
 
-def find_item_by_name_in_collection(name, collection)
+def find_item_by_name_in_collection(name, collection) #Destructive
   # Implement me first!
   # Consult README for inputs and outputs]
    i = 0
-
-
   while i < collection.length
-
     if collection[i][:item] == name
-      a = collection[i]
-      end
+      return  collection[i]
+    end
       i=i+1
     end
-    return a
+    return nil
 end
 
 def consolidate_cart(cart)
@@ -21,28 +18,65 @@ def consolidate_cart(cart)
   i=0
   array =[]
   while i < cart.length
-    if array.include?(cart[i]) #array.include?(cart[0][:item])
-        hash=cart[i]
-        hash[:count]=1exit
-        aindex = array.index(cart[i])
-        array[aindex][:count] += 1
+  #  binding.pry
+    itemname=cart[i][:item]
+    if find_item_by_name_in_collection(itemname,array)
+        #aindex = array.index(find_item_by_name_in_collection(itemname,array))
+        #array[aindex][:count] += 1
+        item = find_item_by_name_in_collection(itemname,array)
+        item[:count] += 1
     else
-      array[i] = cart[i]
-      array [i][:count] = 1
+      cart[i][:count] = 1
+      array << cart[i]
     end
     i=i+1
   end
   return array
 end
+
 cart = [{:item=>"AVOCADO", :price=>3.0, :clearance=>true},
  {:item=>"AVOCADO", :price=>3.0, :clearance=>true},
  {:item=>"KALE", :price=>3.0, :clearance=>false}]
- puts consolidate_cart(cart)
+
+ccart = consolidate_cart(cart)
+
+ vm = find_item_by_name_in_collection("AVOCADO",ccart)
+ puts vm
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  j=0
+  discountedItems = []
+  array = cart
+      while j<coupons.length do
+          binding.pry
+        item = coupons[j][:item]
+        operator = find_item_by_name_in_collection(item,cart)
+        if find_item_by_name_in_collection(item,cart)
+          numPerCoupon=find_item_by_name_in_collection(item,coupons)[:num]
+          countPerCart = find_item_by_name_in_collection(item,cart)[:count]
+          costPerCoupon = find_item_by_name_in_collection(item,coupons)[:cost]
+          clearance = find_item_by_name_in_collection(item,cart)[:clearance]
+          times_used=countPerCart/numPerCoupon
+          updatedUnitPrice = costPerCoupon/numPerCoupon
+          if numPerCoupon/countPerCart>0
+            operator[:count]=operator[:count]-numPerCoupon*times_used
+
+            newTerm = {
+              :item => "#{item} W/COUPON",
+              :price => updatedUnitPrice,
+              :clearance => clearance,
+              :count => times_used*numPerCoupon
+              }
+            array << newTerm
+          end
+
+        #array[array.index(find_item_by_name_in_collection(item,cart))]
+
+        end
+      j+=1
+    # REMEMBER: This method **should** update cart
+     end
 end
 
 def apply_clearance(cart)
